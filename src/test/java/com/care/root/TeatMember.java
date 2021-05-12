f9c36b17d48d47061969d691d17b2a84f7f9608e
jeongwoo <dahlia11444@gmail.com>
2021-05-12 18:05:59 +0900

    test

--- src/test/java/com/care/root/TeatMember.java
+++ src/test/java/com/care/root/TeatMember.java
@@ -1,5 +1,27 @@
 package com.care.root;
 
-public class TeatMember {
+import static org.junit.Assert.assertNotNull;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.care.root.member.controller.MemberController;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = {"classpath:testMember.xml"}) //특정 위치에있는 파일을 가져온다
+public class TeatMember {
+	@Autowired MemberController mc;
+	@Test
+	public void testMc() {
+		System.out.println("----mc : +mc");
+		assertNotNull(mc);// null이 아니면 성공
+	}
 }
+
+
+
+
+
